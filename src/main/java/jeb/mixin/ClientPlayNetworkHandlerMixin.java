c483a4c065447ee8044bd9ae251da45da798fb81
NewLife <rofumer@gmail.com>
2025-10-08 15:53:59 +0300

    1.21.10

--- src/main/java/jeb/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/jeb/mixin/ClientPlayNetworkHandlerMixin.java
@@ -42,7 +42,8 @@ public abstract class ClientPlayNetworkHandlerMixin {
             "1.21.6", 1395,
             "1.21.7", 1395,
             "1.21.8", 1395,
-            "1.21.9", 1449
+            "1.21.9", 1449,
+            "1.21.10", 1449
     );
 
     @Unique
@@ -52,7 +53,8 @@ public abstract class ClientPlayNetworkHandlerMixin {
             "1.21.6", 262,
             "1.21.7", 262,
             "1.21.8", 262,
-            "1.21.9", 283
+            "1.21.9", 283,
+            "1.21.10", 283
     );
 
     @Inject(
